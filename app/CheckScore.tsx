6d86222942a311dd29d4417f9e7c3fbed7470110
Manu Bhadoria <nihilycho@gmail.com>
2024-02-14 23:26:21 +0530

    Update CheckScore.tsx

--- app/CheckScore.tsx
+++ app/CheckScore.tsx
@@ -46,7 +46,7 @@ const CheckScoreContainer = styled('div')`
 `;
 
 const getColorByScore = (score) => {
-  if (score >= 90) return '#4CFF08'; 
+  if (score >= 90) return '#9FFF9F'; 
   if (score >= 80) return '#90EE90'; 
   if (score >= 70) return '#CCCC00'; 
   if (score >= 60) return '#FFFFE0'; 
