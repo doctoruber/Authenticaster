22dde4c71a3727e7da55133b809e53039a88f0c7
Manu Bhadoria <nihilycho@gmail.com>
2024-02-14 23:00:08 +0530

    Update CheckScore.tsx

--- app/CheckScore.tsx
+++ app/CheckScore.tsx
@@ -6,11 +6,9 @@ import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import axios from 'axios';
-import '@react95/icons/icons.css';
-import Navbar from './NavBar'; 
-import { useSpring, animated } from 'react-spring'; 
-import styled, { keyframes } from 'styled-components'; 
-import { Buffer } from 'buffer';
+import Navbar from './NavBar';
+import { useSpring, animated } from 'react-spring';
+import styled, { keyframes } from 'styled-components';
 
 window.Buffer = Buffer;
 
@@ -19,13 +17,9 @@ const theme = createTheme({
     mode: 'dark',
   },
   typography: {
-    fontFamily: [
-      '"Roboto Mono"',
-      'monospace',
-    ].join(','),
+    fontFamily: ['"Roboto Mono"', 'monospace'].join(','),
   },
 });
-
 
 const fadeIn = keyframes`
   from {
@@ -38,7 +32,6 @@ const fadeIn = keyframes`
   }
 `;
 
-
 const CheckScoreContainer = styled('div')`
   background: linear-gradient(270deg, #000, #000);
   background-size: 400% 400%;
@@ -50,11 +43,21 @@ const CheckScoreContainer = styled('div')`
   flex-direction: column;
   justify-content: center;
 `;
+
+const getColorByScore = (score) => {
+  if (score >= 90) return '#006400'; 
+  if (score >= 80) return '#90EE90'; 
+  if (score >= 70) return '#CCCC00'; 
+  if (score >= 60) return '#FFFFE0'; 
+  if (score >= 50) return '#FFB6C1';
+  return '#8B0000';
+};
+
 const ScoreDisplay = styled(animated.div)`
   margin-top: 20px;
   padding: 20px;
   background-color: #222;
-  color: #0f0; // A greenish hue for better visibility
+  color: ${(props) => getColorByScore(props.score)};
   font-size: 24px;
   font-weight: bold;
   text-shadow: 0px 0px 8px rgba(255, 255, 255, 0.8);
@@ -73,75 +76,68 @@ const Content = styled('div')`
 `;
 
 const CheckScore: React.FC = () => {
-    const titleAnimation = useSpring({ opacity: 1, from: { opacity: 0 } });
-    const [launchcasterUrl, setLaunchcasterUrl] = useState('');
-    const [title, setTitle] = useState('');
-    const [time, setTime] = useState('');
-    const [votes, setVotes] = useState(0);
-    const [reliabilityScore, setReliabilityScore] = useState('');
-    const scoreAnimation = useSpring({ opacity: 1, from: { opacity: 0 } });
+  const titleAnimation = useSpring({ opacity: 1, from: { opacity: 0 } });
+  const [launchcasterUrl, setLaunchcasterUrl] = useState('');
+  const [reliabilityScore, setReliabilityScore] = useState('');
+  const [scoreValue, setScoreValue] = useState(0);
+  const scoreAnimation = useSpring({ opacity: 1, from: { opacity: 0 } });
 
-    const fetchData = async () => {
-      if (!launchcasterUrl) return; 
-    
-      const functionUrl = 'https://main--authenticaster.netlify.app/.netlify/functions/fetchLaunchcasterData';
+  const fetchData = async () => {
+    if (!launchcasterUrl) return;
 
-      const encodedUrl = encodeURIComponent(launchcasterUrl);
-      try {
-          const response = await axios.get(`${functionUrl}?url=${encodedUrl}`);
-          const data = response.data;
-          console.log(data);
-          setTitle(data.title);
-          setTime(data.time);
-          setVotes(data.votes);
-          setReliabilityScore(`Reliability Score: ${parseFloat(data.reliabilityScore).toFixed(2)}%`);
-        
-      } catch (error) {
-          console.error('Error fetching data:', error);
-      }
-    };
-    
+    const functionUrl = 'https://main--authenticaster.netlify.app/.netlify/functions/fetchLaunchcasterData';
+    const encodedUrl = encodeURIComponent(launchcasterUrl);
+    try {
+      const response = await axios.get(`${functionUrl}?url=${encodedUrl}`);
+      const data = response.data;
+      const score = parseFloat(data.reliabilityScore);
+      setScoreValue(score); // Set numerical score for styling
+      setReliabilityScore(`Reliability Score: ${score.toFixed(2)}%`);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
-    const handleSubmit = (event: React.FormEvent) => {
-        event.preventDefault(); 
-        fetchData();
-    };
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    fetchData();
+  };
 
-    return (
-      <ThemeProvider theme={theme}>
-          <Navbar />
-          <CssBaseline />
-          <CheckScoreContainer>
-              <Grid container spacing={3} style={{ padding: 24 }}>
-                  <Grid item xs={12}>
-                      <animated.div style={titleAnimation}>
-                          <Typography variant="h3" component="h1" gutterBottom>
-                              Hi. This is Authenticaster!
-                          </Typography>
-                      </animated.div>
-                      <Content>
-                          <form onSubmit={handleSubmit}>
-                              <TextField 
-                                  label="Launchcaster Post URL" 
-                                  variant="outlined" 
-                                  fullWidth 
-                                  value={launchcasterUrl}
-                                  onChange={(e) => setLaunchcasterUrl(e.target.value)}
-                              />
-                              <Button type="submit" variant="contained" style={{ marginTop: '20px' }}>
-                                  Verify Post
-                              </Button>
-                          </form>
-                          {reliabilityScore && (
-                              <ScoreDisplay style={scoreAnimation}>
-                                  {reliabilityScore}
-                              </ScoreDisplay>
-                          )}
-                      </Content>
-                  </Grid>
-              </Grid>
-          </CheckScoreContainer>
-      </ThemeProvider>
+  return (
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      <CssBaseline />
+      <CheckScoreContainer>
+        <Grid container spacing={3} style={{ padding: 24 }}>
+          <Grid item xs={12}>
+            <animated.div style={titleAnimation}>
+              <Typography variant="h3" component="h1" gutterBottom>
+                Hi. This is Authenticaster!
+              </Typography>
+            </animated.div>
+            <Content>
+              <form onSubmit={handleSubmit}>
+                <TextField
+                  label="Launchcaster Post URL"
+                  variant="outlined"
+                  fullWidth
+                  value={launchcasterUrl}
+                  onChange={(e) => setLaunchcasterUrl(e.target.value)}
+                />
+                <Button type="submit" variant="contained" style={{ marginTop: '20px' }}>
+                  Verify Post
+                </Button>
+              </form>
+              {reliabilityScore && (
+                <ScoreDisplay score={scoreValue} style={scoreAnimation}>
+                  {reliabilityScore}
+                </ScoreDisplay>
+              )}
+            </Content>
+          </Grid>
+        </Grid>
+      </CheckScoreContainer>
+    </ThemeProvider>
   );
 };
 
